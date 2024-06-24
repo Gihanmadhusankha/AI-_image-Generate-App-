71770c18324bd91545c14cd9e7b409ddd8cccadd
Gihanmadhusankha <gmjayaushantha2007@gmail.com>
2024-06-24 09:04:36 +0530

    c1

---
+++ image-generator/src/App.js
@@ -0,0 +1,13 @@
+import imageGenerator from './Components/imageGenerator/imageGenerator';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+    <imageGenerator/>
+    </div>
+  );
+}
+
+export default App;
